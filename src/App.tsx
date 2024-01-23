2574191dda032ff6463904bc98abae7a7bcbb084
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2024-01-23 15:48:08 +0300

    fix: style

--- src/App.tsx
+++ src/App.tsx
@@ -31,14 +31,28 @@ function App() {
     );
 
   return (
-    <div>
-      <ul>
-        <a href={`${import.meta.env.VITE_CV_GEN_URL}?token=${token}`}>CV Gen</a>
-        <a href={`${import.meta.env.VITE_TIME_TRACKER_URL}?token=${token}`}>
+    <div className="bg-slate-900 w-[100dvw] h-[100dvh] text-slate-300">
+      <p className="text-3xl text-center py-6"> MBicycle Foundation</p>
+      <ul className="flex mb-10 gap-10 p-10">
+        <a
+          href={`${import.meta.env.VITE_CV_GEN_URL}?token=${token}`}
+          className="border aspect-square border-slate-500 w-[15rem] flex justify-center items-center rounded-md"
+        >
+          CV Gen
+        </a>
+        <a
+          href={`${import.meta.env.VITE_TIME_TRACKER_URL}?token=${token}`}
+          className="border aspect-square border-slate-500 w-[15rem] flex justify-center items-center rounded-md"
+        >
           CV Time Tracker
         </a>
       </ul>
-      <button onClick={logout}>Logout</button>
+      <button
+        onClick={logout}
+        className="bg-blue-400 text-white px-4 py-2 rounded-md absolute top-10 right-10"
+      >
+        Logout
+      </button>
     </div>
   );
 }
