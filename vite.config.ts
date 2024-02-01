b728a023faaf74e5343a1100b542f99fe5f6279e
PashkevichTaniaMbicycle <pashkevich.tatsiana@mbicycle.com>
2024-02-01 14:43:17 +0300

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -6,5 +7,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      utils: path.resolve(__dirname, 'src/utils'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.svg'],
   },
 });
