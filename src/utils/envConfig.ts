82d2548030a66079e515a0ae20eecf7a40c76719
PashkevichTaniaMbicycle <pashkevich.tatsiana@mbicycle.com>
2024-03-17 23:27:19 +0300

    fix config

--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -6,9 +6,9 @@ type Config = {
 }
 
 const CONFIG: Config = {
-  redirectUri: import.meta.env.VITE_REDIRECT_URL || 'https://localhost:3000/',
-  cvGenUrl: import.meta.env.VITE_CV_GEN_URL || 'https://localhost:3001/',
-  timeTrackerUrl: import.meta.env.VITE_TIME_TRACKER_URL || 'https://localhost:3002/',
+  redirectUri: import.meta.env.VITE_REDIRECT_URL || 'http://localhost:3000/',
+  cvGenUrl: import.meta.env.VITE_CV_GEN_URL || 'http://localhost:3001/',
+  timeTrackerUrl: import.meta.env.VITE_TIME_TRACKER_URL || 'http://localhost:3002/',
   appDomain: import.meta.env.VITE_DOMEN,
 };
 
